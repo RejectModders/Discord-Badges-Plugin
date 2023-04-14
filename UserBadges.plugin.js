dc00aad8abb10ab56f8386eee81c0dbea04833fd
RejectModders <129556630+RejectModders@users.noreply.github.com>
2023-04-14 18:32:43 -0500

    Update UserBadges.plugin.js

--- UserBadges.plugin.js
+++ UserBadges.plugin.js
@@ -37,27 +37,59 @@ class UserBadges {
 
         const token = this.accountDetailsModule.getToken();
         const headers = { 'Authorization': token };
-        const url = `${this.API_URL}/users/${id}/profile`;
+        const url = `${this.API_URL}/users/@me/guilds`;
         const fetchOptions = { method: 'GET', headers: headers };
 
         fetch(url, fetchOptions)
             .then((response) => response.json())
-            .then((data) => {
-                if (data.premium_since) {
-                    this.badges['Nitro'] = { 'name': 'Nitro', 'description': 'Discord Nitro Subscriber', 'image': 'https://discord.com/assets/5ccabf62108d5a8074ddd95af2211727.svg' };
-                }
-
-                this.badgesModule.getBadges().forEach((badge) => {
-                    if (data.profile_badges.includes(badge.id)) {
-                        this.badges[badge.name] = { 'name': badge.name, 'description': badge.description, 'image': badge.image_url };
-                    }
-                });
-
-                this.loaded = true;
-                this.updateBadges();
+            .then((guilds) => {
+                const guildIds = guilds.map((guild) => guild.id);
+                const guildRolesUrl = `${this.API_URL}/guilds/${guildIds.join(',')}/roles`;
+                return fetch(guildRolesUrl, fetchOptions);
             })
-            .catch((error) => {
-                console.error('Unable to fetch user badges', error);
+            .then((response) => response.json())
+            .then((roles) => {
+                const roleBadges = roles
+                    .filter((role) => role.tags && role.tags.includes('DISCORD_NITRO') && !role.managed) // Discord Nitro Booster
+                    .map((role) => {
+                        return { 'name': 'Nitro Booster', 'description': `Discord Nitro Booster - ${role.name}`, 'image': 'https://discord.com/assets/5ccabf62108d5a8074ddd95af2211727.svg' };
+                    });
+
+                const badgePromises = [
+                    fetch(`${this.API_URL}/users/${id}/profile`, fetchOptions), // Profile badges
+                    fetch(`${this.API_URL}/user/${id}/badges`, fetchOptions), // Community badges
+                ];
+
+                Promise.all(badgePromises)
+                    .then((responses) => Promise.all(responses.map((response) => response.json())))
+                    .then(([profileData, communityData]) => {
+                        // Add Nitro Booster badge
+                        roleBadges.forEach((badge) => {
+                            this.badges[badge.name] = badge;
+                        });
+
+                        // Add profile badges
+                        if (profileData.premium_since) {
+                            this.badges['Nitro'] = { 'name': 'Nitro', 'description': 'Discord Nitro Subscriber', 'image': 'https://discord.com/assets/5ccabf62108d5a8074ddd95af2211727.svg' };
+                        }
+                        profileData.profile_badges.forEach((badgeId) => {
+                            const badge = this.badgesModule.getBadges().find((b) => b.id === badgeId);
+                            if (badge) {
+                                this.badges[badge.name] = { 'name': badge.name, 'description': badge.description, 'image': badge.image_url };
+                            }
+                        });
+
+                        // Add community badges
+                        communityData.badges.forEach((badge) => {
+                            this.badges[badge.name] = { 'name': badge.name, 'description': badge.description, 'image': badge.image_url };
+                        });
+
+                        this.loaded = true;
+                        this.updateBadges();
+                    })
+                    .catch((error) => {
+                        console.error('Unable to fetch user badges', error);
+                    });
             });
     }
 
@@ -81,11 +113,12 @@ class UserBadges {
             }
         });
     }
+
     stop() {
         if (this.badgesPluginInitialized) {
-            const userPopout = BdApi.findModuleByProps('UserPopout');
+            const userPopout = this.ZLibrary.DiscordModules.UserPopout;
             if (userPopout) {
-                BdApi.Patcher.unpatch(userPopout.default, 'userBadges');
+                this.ZLibrary.Patcher.unpatch(userPopout.default, 'userBadges');
             }
         }
     }
